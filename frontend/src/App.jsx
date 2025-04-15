09c17f3b1d6346e5bb009516d3a2f52e1a624a64
Majd-Dayoub <majd.dayoub04@gmail.com>
2025-04-15 13:53:30 -0400

    1) Setup Vite and Tailwind in frontend 2) Added logo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <img src="/logo.png" alt="" />
+    </div>
+  );
+}
+
+export default App
